5da1d1b4a59cc951583bf5cfeabe2bd8b1fb6ce3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2016-12-23 17:45:46 +0900

    Implement Low-Pass Filter

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,9 +3,12 @@
     document.addEventListener('DOMContentLoaded', () => {
         const context = new AudioContext();
 
+        const Q = 1 / Math.sqrt(2);
+
+        let filter    = null
+        let frequency = document.getElementById('range-cutoff-frequency').valueAsNumber;
         let buffer    = null;
         let source    = context.createBufferSource();;
-        let iirFilter = null;
         let gain      = context.createGain();
 
         const successCallback = audioBuffer => {
@@ -18,13 +21,34 @@
             source.buffer = audioBuffer;
             source.playbackRate.value = playbackRate;
 
-            source.connect(gain).connect(context.destination);
+            filter = createLPFilter(frequency, Q);
+
+            source.connect(filter).connect(gain).connect(context.destination);
 
             source.start(0);
         };
 
         const errorCallback = error => {
             console.error('decode error');
+        };
+
+        const createLPFilter = (fd, Q) => {
+            const denominators = [];
+            const numerators   = [];
+
+            const fc = Math.tan((Math.PI * fd) / context.sampleRate) / (2.0 * Math.PI);
+
+            const d = 1.0 + ((2.0 * Math.PI * fc) / Q) + (4.0 * Math.pow(Math.PI, 2) * Math.pow(fc, 2));
+
+            denominators[0] = 1.0;
+            denominators[1] = ((8.0 * Math.pow(Math.PI, 2) * Math.pow(fc, 2)) - 2.0) / d;
+            denominators[2] = (1.0 - ((2.0 * Math.PI * fc) / Q) + (4.0 * Math.pow(Math.PI, 2) * Math.pow(fc, 2))) / d;
+
+            numerators[0] = (4.0 * Math.pow(Math.PI, 2) * Math.pow(fc, 2)) / d;
+            numerators[1] = (8.0 * Math.pow(Math.PI, 2) * Math.pow(fc, 2)) / d;
+            numerators[2] = (4.0 * Math.pow(Math.PI, 2) * Math.pow(fc, 2)) / d;
+
+            return context.createIIRFilter(numerators, denominators);
         };
 
         document.querySelector('[type="file"]').addEventListener('change', event => {
@@ -53,6 +77,16 @@
         document.getElementById('range-playback-rate').addEventListener('input', event => {
             source.playbackRate.value = event.currentTarget.valueAsNumber;
             document.getElementById('output-playback-rate').textContent = event.currentTarget.value;
+        }, false);
+
+        document.getElementById('range-cutoff-frequency').addEventListener('input', event => {
+            frequency = event.currentTarget.valueAsNumber;
+            filter    = createLPFilter(frequency, Q);
+
+            source.disconnect(0);
+            source.connect(filter).connect(gain).connect(context.destination);
+
+            document.getElementById('output-cutoff-frequency').textContent = event.currentTarget.value;
         }, false);
 
     }, true);
